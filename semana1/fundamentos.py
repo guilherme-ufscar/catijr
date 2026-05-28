ffa2cd41f080955e20fabd8320c3f783d1246d29
Guilherme Silva <guilherme.rampazzo@estudante.ufscar.br>
2026-05-27 23:08:36 -0300

    feat: Semana 1 - Fundamentos Básicos

---
+++ semana1/fundamentos.py
@@ -0,0 +1,10 @@
+nome = 'Guilherme' #string
+idade = 19 #int
+aulas_vistas = ['fundamento','pandas']
+terminou_curso = False
+
+print(f'Nome: {nome}')
+print('Nome:', nome)
+print('Nome: {}'.format(nome))
+
+type(19.5)
